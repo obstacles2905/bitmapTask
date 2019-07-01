f63619d6bdf7967732f67e4fc212777d185daa52
nikitapismenniy <nikita.pismenniy@together.com>
2019-07-01 11:42:06 +0300

    get data from input, data validation

--- controllers/BitmapController.js
+++ controllers/BitmapController.js
@@ -1,4 +1,5 @@
 "use strict";
+const readline = require("readline");
 
 /**
  * Bitmap class implementation
@@ -6,66 +7,172 @@
 class BitmapController {
 
   /**
-   * Questions showing in the console
-   * @returns {string[]}
-   */
-  get questions() {
-    return [
-      "Input the number of bitmaps to generate: ",
-      "Input the bitmap width: ",
-      "Input the bitmap length: "
-    ];
-  }
-
-  /**
-   * Main function, shows bitmaps and distances for each pixel
+   * Main function, asks questions, shows bitmaps and distances for each pixel
    *
    * @returns undefined
    */
-  showResult() {
-    process.stdin.setEncoding("utf8");
-
-    const answers = [];
-
-    console.log(this.questions[0]);
-
-    process.stdin.on("data", (data) => {
-
-      answers.push(Number(data.trim()));
-
-      if (answers.length < this.questions.length) {
-        console.log(this.questions[answers.length])
-      } else {
-
-        const bitmaps = this._getBitmaps(answers);
-        bitmaps.forEach(bitmap => {
-          const distances = this._getDistances(bitmap);
-          const bitmapHeight = answers[2];
-
-          this._showResult(bitmap, distances, bitmapHeight);
-        });
-        process.exit();
-      }
+  async showResult() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
     });
 
-    process.on("exit", () => {
-      console.log("end \n")
-    })
+    const question = (str) => new Promise(resolve => rl.question(str, resolve));
+    const steps = {
+      start: async() => {
+        console.log("Welcome to the bitmap distance app")
+      },
+      getAmount: async() => {
+        const amount = async() => {
+          const answer = await question("Input the number of bitmaps: ");
+
+          if (!Number(answer)) {
+            console.error("Incorrect amount. The value must be a number");
+            await amount();
+          }
+
+          return answer;
+        };
+
+        return await amount();
+      },
+      getWidth: async() => {
+        const width = async() => {
+          const answer = await question("Input the bitmap width: ");
+
+          if (!Number(answer)) {
+            console.error("Incorrect width. The value must be a number");
+            await width();
+          }
+
+          return answer;
+        };
+
+        return await width();
+      },
+      getHeight: async() => {
+        const height = async() => {
+          const answer = await question("Input the bitmap height: ");
+
+          if (!Number(answer)) {
+            console.error("Incorrect height. The value must be a number");
+            await height();
+          }
+
+          return answer;
+        };
+
+        return await height();
+      },
+      getGenerationWay: async() => {
+        const generationWay = async() => {
+          const answer = Number(await question(`Do you want the data to be generated randomly
+          or by input? (0 - input, 1 - random): `));
+
+          if (!this._isValidData(answer)) {
+            console.error("Incorrect answer. The value must be either 0 or 1");
+            await generationWay();
+          }
+
+          return answer;
+        };
+
+        return await generationWay();
+      },
+      end: () => {
+        rl.close();
+      }
+    };
+
+    steps.start();
+    const amount = await steps.getAmount();
+    const width = await steps.getWidth();
+    const height = await steps.getHeight();
+    const generationWay = Number(await steps.getGenerationWay(amount, width, height));
+
+    generationWay === 0
+      ? this._getInputBitmaps(amount, width, height)
+      : this._getGeneratedBitmaps(amount, width, height);
+
+  }
+
+  /**
+   * Transform and show input bitmap data
+   *
+   * @param {string} amount of bitmaps to generate
+   * @param {string} width number of columns
+   * @param {string} height number of rows
+   * @private
+   */
+  _getInputBitmaps(amount, width, height) {
+    const arraySize = width*height;
+
+    console.log(`Input ${width*height} numbers from 0 to 1`);
+    const inputData = [];
+
+    process.stdin.on("data", (data) => {
+      if (this._isValidData(data)) {
+        inputData.push(Number(data));
+
+        if (inputData.length < arraySize) {
+          console.log("Input another number");
+        } else {
+
+          const bitmaps = this._getBitmaps(amount, width, height, false, inputData);
+          bitmaps.forEach(bitmap => {
+            const distances = this._getDistances(bitmap);
+            this._showResult(bitmap, distances, height);
+          });
+        }
+      } else {
+        console.error("Incorrect data, the value must be either 0 or 1")
+      }
+    });
+  }
+
+  /**
+   * Checks if data is valid
+   *
+   * @param {number} element to check
+   * @returns {boolean} true if the element is 0 or 1
+   * @private
+   */
+  _isValidData(element) {
+    return Number(element) === 0 || Number(element) === 1;
+  }
+
+  /**
+   * Generates bitmaps and shows result to console
+   *
+   * @param {number} amount of bitmaps to generate
+   * @param {number} width number of columns
+   * @param {number} height number of rows
+   * @private
+   */
+  _getGeneratedBitmaps(amount, width, height) {
+    const bitmaps = this._getBitmaps(amount, width, height, true);
+    bitmaps.forEach(bitmap => {
+      const distances = this._getDistances(bitmap);
+
+      this._showResult(bitmap, distances, height);
+    });
   }
 
   /**
    * Get array of generated bitmaps
    *
-   * @param {array} answers object containing input information
+   * @param {string} amount of bitmaps to be generated
+   * @param {string} width of bitmap
+   * @param {string} height of bitmap
+   * @param {boolean} isGenerated means data should be randomly generated
+   * @param {number} value pixel value
    * @returns {Array} generated bitmaps
    * @private
    */
-  _getBitmaps(answers) {
-    const [bitmapsAmount, width, height] = answers;
-
+  _getBitmaps(amount, width, height, isGenerated, value) {
     const bitmaps = [];
-    for (let i = 0; i < bitmapsAmount; i++) {
-      bitmaps.push(this._getBitmap(width, height));
+    for (let i = 0; i < amount; i++) {
+      bitmaps.push(this._getBitmap(width, height, isGenerated, value));
     }
 
     return bitmaps;
@@ -75,23 +182,31 @@ class BitmapController {
    * Generates one bitmap with random pixels from 0 to 1
    * @param {number} width number of columns
    * @param {number} height number of rows
+   * @param {boolean} isGenerated means data should be randomly generated
+   * @param {number} value pixel value
    * @returns {Array} generated bitmap
    * @private
    */
-  _getBitmap(width, height) {
+  _getBitmap(width, height, isGenerated, value) {
     const result = [];
 
+    let i = 0;
     for (let w = 1; w <= width; w++) {
       for (let h = 1; h <= height; h++) {
-        result.push({width: w, height: h, value: Math.round(Math.random())});
+        if (isGenerated) {
+          result.push({width: w, height: h, value: Math.round(Math.random())});
+        } else {
+          result.push({width: w, height: h, value: value[i]});
+          i++;
+        }
       }
+
     }
 
     const whitePixelExists = result.find(bit => bit.value === 1);
     if (!whitePixelExists) {
       result[0].value = 1;
     }
-
     return result;
   }
 
@@ -105,7 +220,7 @@ class BitmapController {
     const distances = [];
 
     for (let elem in bitmap) {
-      if (bitmap[elem].value !== 0) {
+      if (bitmap[elem].value === 1) {
         distances.push(0);
         continue;
       }
